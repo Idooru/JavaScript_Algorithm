a0ddff6fbb0dfebab8fd4baba834bcb2e181afc4
shere <shere1765@gmail.com>
2022-04-08 11:31:20 +0900

    0.4.0.8

---
+++ myAlgorithm/hashed.js
@@ -0,0 +1,33 @@
+const existArr = [5, 6, 14, 20, 29, 34, 37, 51, 69, 75];
+const hashedArr = [];
+const hashTable = [];
+const hashKey = 13;
+
+console.log(`기존 배열: [${existArr}]`);
+
+for (let i in existArr) {
+    hashedArr[i] = null;
+}
+
+let i = 0;
+
+while (i < hashedArr.length) {
+    for (let j of existArr) {
+        let formula = j % hashKey;
+        hashedArr[i] = formula;
+        i++;
+    }
+}
+
+console.log(`해시화한 배열: [${hashedArr}]`);
+
+i = 0;
+while (i < hashedArr.length) {
+    for (let j of existArr) {
+        let formula = j % hashKey;
+        hashTable[formula] = existArr[i];
+        i++;
+    }
+}
+
+console.log(`해시테이블: [${hashTable}]`);
